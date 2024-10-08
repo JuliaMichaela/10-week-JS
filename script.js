d9819a3b7f8596b452183d6ab7701a04901a9433
JuliaMichaela <juliaperez@yandex.ru>
2024-10-08 16:35:47 +0300

    completed task *

---
+++ script.js
@@ -0,0 +1,17 @@
+// Задание 2 часть 1
+
+function showMessage () {
+    console.log ('Я учу JavaScript!');
+}
+
+showMessage ();
+
+// Задание 2 часть 2
+
+// Задание со звездочкой ***
+
+function calculateTotalPrice(quantity = 2, price = 15000000) {
+    let result = quantity * price;
+    alert (`Стоимость покупки: ${result.toLocaleString('ru-RU')} рублей`)
+}
+calculateTotalPrice ()
