442c7d88fe796cb2a337fddde657b2cee90f0fb9
JuliaMichaela <juliaperez@yandex.ru>
2024-10-10 16:03:16 +0300

    edited script.js

--- script.js
+++ script.js
@@ -7,25 +7,26 @@ function showMessage () {
 showMessage ();
 
 // Задание 2 часть 2
+
 const image = document.getElementById('image');
 const nextButton = document.getElementById('next');
 const previousButton = document.getElementById('previous');
 
 function showNext () {
-    image.src = './assets/images/img2.jpeg'
-}
-function showPrevious () {
-    image.src = './assets/images/img1.jpg'
+    image.src = './assets/images/img2.jpeg';
 }
 
+function showPrevious () {
+    image.src = './assets/images/img1.jpg';
+}
 
 
 // Задание со звездочкой ***
-// const buttonPrice = document.getElementById('buttonPrice');
-// function calculateTotalPrice(quantity = 2, price = 15000000) {
-//     let result = quantity * price;
+const buttonPrice = document.getElementById('buttonPrice');
+const contentPrice = document.getElementById ('content');
 
-//     buttonPrice.textContent= '`Стоимость покупки: ${result.toLocaleString('ru-RU')} рублей`'
+function calculateTotalPrice(quantity = 2, price = 15000000) {
+    let result = quantity * price;
+    contentPrice.textContent= `Стоимость покупки: ${result.toLocaleString('ru-RU')} рублей.`
+}
 
-// }
-// calculateTotalPrice ()
